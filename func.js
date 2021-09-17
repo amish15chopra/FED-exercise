dc2cc194bd5ad0774f25a6f7e35ece215e79b767
Amish Chopra <amish15chopra@gmail.com>
2021-09-17 17:34:22 +0530

    almost complete

---
+++ func.js
@@ -0,0 +1,16 @@
+function openNav() {
+    document.getElementById("sidebar").style.display = "block";
+}
+function closeNav() {
+    document.getElementById("sidebar").style.display = "none";
+}
+function showNodal() {
+    document.getElementById("modal-box").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("modal-box").style.display = "none";
+}
+function toggleLogoutBtn() {
+    document.getElementById("logout-btn").style.display = "block";
+}
