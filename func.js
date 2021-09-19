36872b934364100105777012cf9dc198aef35e2f
Amish Chopra <amish15chopra@gmail.com>
2021-09-19 23:46:19 +0530

    Completed and added the todo file

--- func.js
+++ func.js
@@ -12,5 +12,23 @@ function closeModal() {
     document.getElementById("modal-box").style.display = "none";
 }
 function toggleLogoutBtn() {
-    document.getElementById("logout-btn").style.display = "block";
+    var x =  document.getElementById("logout-btn");
+    if(x.style.display == "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+function showProductInfo() {
+    document.getElementsByClassName("desc-text-product-description")[0].style.display = "none";
+    document.getElementsByClassName("desc-text-product-info")[0].style.display = "block";
+    document.getElementsByClassName("info")[0].style.borderBottom = "4px solid #007bff";
+    document.getElementsByClassName("desc")[0].style.borderBottom = "none";
+}
+function showProductDescription() {
+    document.getElementsByClassName("desc-text-product-description")[0].style.display = "block";
+    document.getElementsByClassName("desc-text-product-info")[0].style.display = "none";
+    document.getElementsByClassName("desc")[0].style.borderBottom = "4px solid #007bff";
+    document.getElementsByClassName("info")[0].style.borderBottom = "none";
 }
